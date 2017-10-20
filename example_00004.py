e6bf4ac8918a79234f4be5554537243c9b1c9f31
RichardRhee <jungjinrichardslee@gmail.com>
2017-10-20 21:15:49 +0900

    python examples day 1

---
+++ example_00004.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 20:55:13 2017
+
+@author: Jung Jin Lee
+
+example 00004
+
+"""
+
+hello = 'hello'    # String literals can use single quotes
+world = "world"    # or double quotes; it does not matter.
+print(hello)       # Prints "hello"
+print(len(hello))  # String length; prints "5"
+hw = hello + ' ' + world  # String concatenation
+print(hw)  # prints "hello world"
+hw12 = '%s %s %d' % (hello, world, 12)  # sprintf style string formatting
+print(hw12)  # prints "hello world 12"
+
+s = "hello"
+print(s.capitalize())  # Capitalize a string; prints "Hello"
+print(s.upper())       # Convert a string to uppercase; prints "HELLO"
+print(s.rjust(7))      # Right-justify a string, padding with spaces; prints "  hello"
+print(s.center(7))     # Center a string, padding with spaces; prints " hello "
+print(s.replace('l', '(ell)'))  # Replace all instances of one substring with another;
+                                # prints "he(ell)(ell)o"
+print('  world '.strip())  # Strip leading and trailing whitespace; prints "world"
